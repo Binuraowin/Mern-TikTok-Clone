d43cd8a390c3a946107fe3d60009697ce0d38bf5
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-16 12:27:59 +0530

    modules

--- tiktok_clone/src/VideoFooter.js
+++ tiktok_clone/src/VideoFooter.js
@@ -4,6 +4,13 @@ import './VideoFooter.css'
 function VideoFooter() {
     return(
         <div className="videoFooter">
+            <div className="video_footer_text">
+                <h3>Binuraowin</h3>
+                <p>Description</p>
+
+            </div>
+
+
 
         </div>
     )
