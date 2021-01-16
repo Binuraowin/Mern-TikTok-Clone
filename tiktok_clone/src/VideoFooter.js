f959640e1f8a6fe5556244980e8b0849f4cafb45
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-16 14:58:49 +0530

    materialicon

--- tiktok_clone/src/VideoFooter.js
+++ tiktok_clone/src/VideoFooter.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './VideoFooter.css'
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import Ticker from 'react-ticker'
 
 function VideoFooter() {
     return(
@@ -7,8 +9,23 @@ function VideoFooter() {
             <div className="video_footer_text">
                 <h3>Binuraowin</h3>
                 <p>Description</p>
-
+                <div className="videoFooter_ticker">
+                    <MusicNoteIcon className="videoFooter_icon"/>
+                    <Ticker mode="smooth">
+                        {
+                            ({index})=>(
+                                <>
+                                <p>Song</p>
+                                </>
+                            )
+                        }
+                    </Ticker>
+                </div>
             </div>
+            <img className="videoFooter_record"
+            src="link"
+                 alt="hfghj"
+            />
 
 
 
